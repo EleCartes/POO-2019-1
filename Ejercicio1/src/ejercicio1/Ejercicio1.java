97ede08d392fdca18a4a8e5e811210b2c4774592
Nelson Ganga <nelson.ganga.inacap@outlook.com>
2019-03-11 10:01:58 -0300

    Peso ideal

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -5,9 +5,14 @@
  */
 package ejercicio1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author fredy
+ * EJERCICIO 1: Elabore una aplicación de consola Java que permita determinar el peso ideal
+ * de un niño o niña utilizando la siguiente fórmula: edad * 2 + 8
+ * Muestre en pantalla el resultado.
  */
 public class Ejercicio1 {
 
@@ -15,7 +20,28 @@ public class Ejercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Paso 1: Solicitamos al usuario la edad del niño/a
+        System.out.print("Ingrese edad : ");
+        
+ 
+        // Paso 2: Utilizamos la clase Scanner para obtener datos ingresados.
+        Scanner objetoEntrada = new Scanner(System.in);
+        
+        // Paso 3: Capturamos la edad y la asignamos a una variable
+        //Declaramos de la siguiente forma: [Tipo de dato] [nombre variable]
+        int edad = objetoEntrada.nextInt();
+        
+        /* Paso 4: Efectuamos la operación de cómputo
+         utilizando la fórmula del ejercicio. Para
+         esto declaramos una variable que almacene
+         el peso ideal */
+        int pesoIdeal = edad * 2 + 8;
+        
+        /* Paso 5: Mostramos el resultado*/
+        System.out.println("El peso ideal es "+ pesoIdeal);
+        
+        
+        
     }
     
 }
