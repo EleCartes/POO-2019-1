a94469cce97b8bbd5548dc7213b686fdb4a3d4cc
Nelson Ganga <nelson.ganga.inacap@outlook.com>
2019-03-25 10:37:44 -0300

    Arreglos de diferente tipo

--- Arreglos2/src/arreglos2/Arreglos2.java
+++ Arreglos2/src/arreglos2/Arreglos2.java
@@ -5,9 +5,15 @@
  */
 package arreglos2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author fredy
+ * Creamos 2 arreglos de tipo String e int para capturar
+ * nombre y edad de diferentes personas.
+ * El tamaño de los arreglos es dinámico ya que solicitamos
+ * al usuario la cantidad de registros a ingresar.
  */
 public class Arreglos2 {
 
@@ -15,7 +21,41 @@ public class Arreglos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Solicitamos al usuario la cantidad de registros a ingresar
+        System.out.print("Ingrese cantidad de registros a ingresar : ");
+        //Creamos objeto de la clase Scanner para capturar
+        Scanner entrada = new Scanner(System.in);
+        
+        //Creamos variable y asignamos lo que el usuario ingresa
+        int cantidad = entrada.nextInt();
+        //Creamos variables adicionales
+        String nombre;
+        int edad;
+        //Creamos los arreglos
+        String nombres[]    = new String[cantidad];
+        int edades[]        = new int[cantidad];
+        
+        //Creamos estructura de repetición para capturar valores
+        for(int i = 0 ; i < cantidad;i++){
+            //Solicitamos al usuario que ingrese nombre
+            System.out.print("Ingrese nombre persona " + i+1+" : ");
+            Scanner entradaNombres = new Scanner(System.in);
+            //Capturamos el nombre
+            nombre       = entradaNombres.nextLine();
+            //Incorporamos el valor al arreglo de nombres
+            nombres[i]   = nombre;
+            
+
+            //Solicitamos al usuario que ingrese edad
+            System.out.print("Ingrese edad de persona "+ i+1+" :" );
+            //Capturamos la edad
+            edad         = entrada.nextInt();
+            //incorporamos el valor al arreglo de edades
+            edades[i]    = edad;      
+            
+        }
+        
+        
     }
     
 }
