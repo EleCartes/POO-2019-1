21a217d24c73d235b57d391f03ad5da940eb314c
Nelson Ganga <nelson.ganga.inacap@outlook.com>
2019-03-26 19:44:20 -0300

    Ejercicios con arreglos unidimensionales

--- Arreglos2/src/arreglos2/Arreglos2.java
+++ Arreglos2/src/arreglos2/Arreglos2.java
@@ -21,6 +21,12 @@ public class Arreglos2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Variables globales
+        int suma =0;
+        double promedio;
+        int edadBaja;
+        int edadAlta;
+        int indiceMayor =0;
         //Solicitamos al usuario la cantidad de registros a ingresar
         System.out.print("Ingrese cantidad de registros a ingresar : ");
         //Creamos objeto de la clase Scanner para capturar
@@ -38,7 +44,7 @@ public class Arreglos2 {
         //Creamos estructura de repetición para capturar valores
         for(int i = 0 ; i < cantidad;i++){
             //Solicitamos al usuario que ingrese nombre
-            System.out.print("Ingrese nombre persona " + i+1+" : ");
+            System.out.print("Ingrese nombre persona " + (i+1)+" : ");
             Scanner entradaNombres = new Scanner(System.in);
             //Capturamos el nombre
             nombre       = entradaNombres.nextLine();
@@ -47,13 +53,46 @@ public class Arreglos2 {
             
 
             //Solicitamos al usuario que ingrese edad
-            System.out.print("Ingrese edad de persona "+ i+1+" :" );
+            System.out.print("Ingrese edad de persona "+ (i+1)+" :" );
             //Capturamos la edad
             edad         = entrada.nextInt();
             //incorporamos el valor al arreglo de edades
             edades[i]    = edad;      
             
         }
+        // La edad promedio
+        for(int i=0; i < cantidad ;i++){
+            //Sumamos los valores del arreglo
+            suma = suma + edades[i];
+        }
+        //Desplegamos el promedio
+        promedio = suma / cantidad;
+        System.out.println("Promedio : "+ promedio);
+        
+
+        //La edad más baja
+        edadBaja = edades[0];
+        for(int i=0; i < cantidad ;i++){
+            //Comparamos para determinar si hay otra edad mas baja
+            if(edadBaja > edades[i]){
+                edadBaja = edades[i];
+            }
+        }
+        //Mostramos los resultados con la edad mas baja
+        System.out.println("La edad más baja es "+ edadBaja);
+        
+        //Determinar el nombre del mayor
+        edadAlta = edades[0];
+        for(int i =0;i<cantidad;i++){
+            //Determinamos la edad mayor
+            if(edadAlta < edades[i]){
+                edadAlta = edades[i];
+                indiceMayor = i;
+            }     
+        }
+        //Desplegamos el nombre del mayor
+        System.out.println("Nombre del mayor es " + nombres[indiceMayor]);
+        
         
         
     }
